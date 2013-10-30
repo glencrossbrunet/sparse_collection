e2b6692e15c408efbaa95e0c6c5344e262c69c69
aj0strow <alexander.ostrow@gmail.com>
2013-10-30 14:39:33 -0400

    left right and middle riemann sums

--- lib/sparse_datetime.rb
+++ lib/sparse_datetime.rb
@@ -7,7 +7,8 @@ module SparseDatetime
     attr_accessor :sparse_attribute
     
     def sparse(attribute = :created_at)
-      Collection.new self, attribute
+      resources = where.not(attribute => nil).order("#{attribute} ASC")
+      Collection.new resources, attribute
     end
   end
   
@@ -18,5 +19,70 @@ module SparseDatetime
       self.resources = resources
       self.attribute = attribute
     end
+    
+    def self.avg(datetime_range)
+      samples = where(sampled_at: datetime_range).order('sampled_at ASC')
+    
+      if samples.any?
+        total_seconds = datetime_range.end.to_i - samples.first.sampled_at.to_i    
+        average = 0.0
+    
+        samples.each_cons(2) do |start, stop|
+          seconds = stop.sampled_at.to_i - start.sampled_at.to_i
+          average += (seconds.to_f / total_seconds) * start.value
+        end
+
+        seconds = datetime_range.end.to_i - samples.last.sampled_at.to_i
+        average += (seconds.to_f / total_seconds) * samples.last.value
+    
+        average
+      else
+        nil
+      end
+    end
+    
+    def riemann_left(field)
+      return nil unless resources.any?
+      
+      period = (resources.last[attribute] - resources.first[attribute]).to_f
+      total = 0.0
+      
+      resources.each_cons(2).map do |previous, current|
+        sub_period = (current[attribute] - previous[attribute]).to_f
+        total += previous[field] * sub_period
+      end
+      
+      total / period
+    end
+    
+    def riemann_right(field)
+      return nil unless resources.any?
+      
+      period = (resources.last[attribute] - resources.first[attribute]).to_f
+      total = 0.0
+      
+      resources.each_cons(2).map do |previous, current|
+        sub_period = (current[attribute] - previous[attribute]).to_f
+        total += current[field] * sub_period
+      end
+      
+      total / period
+    end
+    
+    def riemann_middle(field)
+      return nil unless resources.any?
+      
+      period = (resources.last[attribute] - resources.first[attribute]).to_f
+      total = 0.0
+            
+      resources.each_cons(2).map do |previous, current|
+        sub_period = (current[attribute] - previous[attribute]).to_f
+        multiplier = sub_period / 2
+        total += multiplier * (previous[field] + current[field])
+      end
+      
+      total / period
+    end
+    
   end
 end
--- spec/sparse_datetime/base_spec.rb
+++ spec/sparse_datetime/base_spec.rb
@@ -2,16 +2,46 @@ require 'spec_helper'
 
 describe Resource do
   describe '#sparse' do
-    let(:resources) { Resource.all }
-    
-    describe 'defaults to created_at' do
-      subject { resources.sparse.attribute }
-      it { should eq(:created_at) }
+    subject { Resource.all.sparse }
+    it { should be_a(SparseDatetime::Collection) }
+  end
+  
+  describe '#riemann' do
+    let(:resources) do
+      resources = [
+        { recorded_on: Date.parse('Jan 1, 2013'), int_value: 0 },
+        # 1 day gap
+        { recorded_on: Date.parse('Jan 2, 2013'), int_value: 5 },
+        # 2 day gap
+        { recorded_on: Date.parse('Jan 4, 2013'), int_value: 10 }
+      ].map { |attributes| Resource.create(attributes) }
+      Resource.where(id: resources.map(&:id))
     end
     
-    describe 'with field' do
-      subject { resources.sparse(:recorded_at).attribute }
-      it { should eq(:recorded_at) }
+    let(:sparse) do
+      resources.sparse(:recorded_on)
+    end
+    
+    let(:delta) do
+      0.00000001
+    end
+    
+    describe 'right' do
+      subject { sparse.riemann_right(:int_value) }
+      sum = (0 * 0) + (1 * 5) + (2 * 10)
+      it { should be_within(delta).of(sum / 3.0) }
+    end
+    
+    describe 'left' do
+      subject { sparse.riemann_left(:int_value) }
+      sum = (1 * 0) + (2 * 5) + (0 * 10)
+      it { should be_within(delta).of(sum / 3.0) }
+    end
+    
+    describe 'middle' do
+      subject { sparse.riemann_middle(:int_value) }
+      sum = (0.5 * 0) + (0.5 * 5) + (1 * 5) + (1 * 10)
+      it { should be_within(delta).of(sum / 3.0) }
     end
   end
 end
